877b0339fd08d3099075a8e67683fe5ef252e024
fshahinfar1 <fshahinfar1@gmail.com>
2019-05-28 21:37:27 +0430

    Implement corss over
    the main function is completed to
    be run without mutation yet.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 
 #include <iostream>
+#include <stdexcept>
 #include <cstdlib>
 #include "Entity.hpp"
 using namespace std;
@@ -16,7 +17,7 @@ using namespace std;
 
 /*
 *   calculates a fittness value between 1 and 0
-*   0 is the best fit, meaning no threats.
+*   1 is the best fit, meaning no threats.
 */
 float  fittness_function(Entity &e) {
     int threats = 0;
@@ -39,13 +40,16 @@ float  fittness_function(Entity &e) {
             }
         }
     }
-    return threats / (float)total_posible_threats;
+    return 1 - (threats / (float)total_posible_threats);
 }
 
 
 /*
 *   This function selects parents for generating the
 *   next population.
+*   
+*   Returns an array of Entity with the same size given
+*   to the function.
 */
 Entity* select_from_population(Entity* population, const int size) {
     float comulative_f[size];
@@ -78,24 +82,103 @@ Entity* select_from_population(Entity* population, const int size) {
     return selected;
 }
 
-int main(int argc, char* argv[]) {
-    // Generate the first population
-    const int population_size = 10;
-    Entity population[population_size];
+
+/*
+*   Applies cross over operation on the population.
+*   Each entity in even position will be cross overed
+*   with next entity.
+*   There is one cross over point.
+*
+*   Returns an array with length = size / 2
+*/
+Entity* cross_over(Entity* population, const int size) {
+    if (size % 2 != 0)
+        throw invalid_argument("population size is not even number!");
+    Entity* result = new Entity[size / 2];
+    int pop_index = 0;
+    for (int i = 0; i < size; i += 2) {
+        Entity* xover = new Entity();
+        int xover_pnt = rand() % Entity::COUNT_COLUMN;
+        for (int j = 0; j < Entity::COUNT_COLUMN; j++) {
+            int val;
+            if (j <= xover_pnt) {
+                val = population[i].get_column(j);
+            } else {
+                val = population[i + 1].get_column(j);
+            }
+            xover->set_column_row(j, val);
+        }
+        result[pop_index++] = *xover;
+    }
+    return result;
+}
+
+
+Entity* generate_random_population(int size) {
+    Entity* population =new Entity[size];
     Entity* e;
-    for (int i = 0; i < population_size; i++) {
+    for (int i = 0; i < size; i++) {
         e = Entity::generate_random_entity();
         population[i] = *e;
-        // cout << e->to_string() << endl;
     }
-    // cout << "++++++++++++++" << endl;
+    return population;
+}
+
+
+Entity* concat_population(Entity* p1, int size1, Entity* p2, int size2) {
+    Entity* res = new Entity[size1 + size2];
+    for (int i = 0; i < size1; i++) {
+        res[i] = p1[i];
+    }
+    for (int i = 0; i < size2; i++) {
+        res[size1 + i] = p2[i];
+    }
+    return res;
+}
+
+
+void show_population(Entity* population, const int size) {
+    for (int i = 0; i < size; i++) {
+        cout << population[i].to_string() << endl;
+    }
+    cout << "++++++++++++++" << endl;
+}
+
+int main(int argc, char* argv[]) {
+    // Generate the first population
+    const int population_size = 100;
+    Entity* population = generate_random_population(population_size);
+    // show_population(population, population_size);
     // Continue for some generations
-    const int count_generations = 1;
-    Entity* selected;
+    const int count_generations = 2000;
     for (int generation = 0; generation < count_generations; generation++) {
-        selected = select_from_population(population, population_size);
-        
+        Entity* selected = select_from_population(population, population_size);
+        Entity* new_population = generate_random_population(population_size);
+        Entity* overal_selected = concat_population(selected, population_size, new_population, population_size);
+        delete selected;  // only remove pointer
+        delete new_population; // only remove pointer
+        Entity* xover_population = cross_over(overal_selected, 2 * population_size);
+        delete[] overal_selected; // free memory allocated for all previous population
+        population = xover_population; // no mutation yet
+        // mutate population
+        // find the best fittness value of population
+        float best_f = fittness_function(population[0]);
+        int best_index = 0;
+        for (int i = 1; i < population_size; i++) {
+            float f = fittness_function(population[i]);
+            if (f > best_f) {
+                best_f = f;
+                best_index = i;
+            }
+        }
+        cout << "gen: " << generation <<
+        " best fittness: " << best_f <<
+        " index: " << best_index << endl;
+        if (best_f == 1) {
+            Entity answer = population[best_index];
+            cout << "Answer: \n" << answer.to_string() << endl;
+            break;
+        }
     }
-    
     return 0;
 }
