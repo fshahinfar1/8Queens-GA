730bf81dbf4ace9170bb6628c1fcbee4fd5c20a2
fshahinfar1 <fshahinfar1@gmail.com>
2019-05-28 20:53:08 +0430

    Implement function selecting part of a population

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,9 @@
 
 
 #include <iostream>
+#include <cstdlib>
 #include "Entity.hpp"
 using namespace std;
-
 
 
 /*
@@ -43,23 +43,59 @@ float  fittness_function(Entity &e) {
 }
 
 
+/*
+*   This function selects parents for generating the
+*   next population.
+*/
+Entity* select_from_population(Entity* population, const int size) {
+    float comulative_f[size];
+    float normal_f;
+    float comulative = 0;
+    float total_f = 0;
+    // calculate sum of f value for normalization
+    for (int i = 0; i < size; i++)
+        total_f += fittness_function(population[i]);
+    // generate the commulative value
+    for (int i = 0; i < size; i++) {
+        normal_f = fittness_function(population[i]) / total_f;
+        comulative += normal_f;
+        comulative_f[i] = comulative;
+    }
+    // select parents randomly with
+    // respect to fittness
+    Entity* selected = new Entity[size];
+    double max_rnd = (double)(RAND_MAX) + 1;
+    for (int i = 0; i < size; i++) {
+        double rnd = rand() / max_rnd;
+        int ptr = 0;
+        while (ptr < size - 1) {
+            if (comulative_f[ptr] >= rnd)
+                break;
+            ptr++;
+        }
+        selected[i] = population[ptr];
+    }
+    return selected;
+}
+
 int main(int argc, char* argv[]) {
     // Generate the first population
-    const int population_size = 50;
+    const int population_size = 10;
     Entity population[population_size];
     Entity* e;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < population_size; i++) {
         e = Entity::generate_random_entity();
         population[i] = *e;
+        // cout << e->to_string() << endl;
     }
-    
+    // cout << "++++++++++++++" << endl;
     // Continue for some generations
-    const int count_generations = 100;
+    const int count_generations = 1;
+    Entity* selected;
     for (int generation = 0; generation < count_generations; generation++) {
-        //...        
+        selected = select_from_population(population, population_size);
+        
     }
-    cout << "Hello\n";
-    
     
     return 0;
 }
